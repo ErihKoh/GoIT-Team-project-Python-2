a887073533abe44a98aba74e49b42c345abf6e39
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2022-08-29 11:33:18 +0300

    change db

--- src/helper_v2/helper_v2/settings.py
+++ src/helper_v2/helper_v2/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'helper_v2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd18abk3nfkicb0',
-        'USER': 'fpnanvmzajlefo',
-        'PASSWORD': 'b3a138b5ce2d20c552f088ec927b08992b2ac92cfc8f28069c5ce209408a085e',
-        'HOST': 'ec2-52-31-219-113.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -125,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'helper_v2/static/' )] # - added
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'helper_app/static/' )] # - added
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') # - added  
 MEDIA_URL = '/media/' # - added  
 # Default primary key field type
